4017815f3fc6a8a50ea3bfcc7bb0f2bed815716f
2100400018 <210040018@iitb.ac.in>
2023-09-24 00:18:52 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+// This is a simple JavaScript file named test.js
+
+// Define a function that adds two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Define a function that greets a person
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Example usage of the functions
+const sum = addNumbers(5, 3);
+console.log(`Sum: ${sum}`);
+
+greet("John");
